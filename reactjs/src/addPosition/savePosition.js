7f53892d277e59c4e6e61b7667f566d48086fb0f
Yi Wen <ywen.github@gmail.com>
2020-12-13 16:04:25 -0800

    Add a uuid (#2)

--- reactjs/src/addPosition/savePosition.js
+++ reactjs/src/addPosition/savePosition.js
@@ -1,11 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import store from '../server/store';
 import getEncrypted from '../encryption/getEncrypted';
 import keyStore from '../encryption/keyStore';
+import field from '../position/model';
 
 const savePosition = ({ data }) => {
-  const result = {};
+  const result = { id: uuidv4() };
   const key = keyStore.fetch();
-  ['symbol', 'quantity', 'purchasePrice', 'openDate'].forEach((name) => {
+  field.metaFields.forEach((name) => {
     const value = data.get(name);
     const encrypted = getEncrypted({ key, value });
     result[name] = encrypted;
