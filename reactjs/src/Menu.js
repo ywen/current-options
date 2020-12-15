5f4ead1ae5472c45846440378e1cebdb55f799c0
Yi Wen <hayafirst@protonmail.com>
2020-12-14 21:21:25 -0800

    Import, not working yet

--- reactjs/src/Menu.js
+++ reactjs/src/Menu.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+
+import Immutable from 'immutable';
+
 import './Menu.scss';
 import openModal from './addPosition/openModal';
+import savePosition from './addPosition/savePosition';
+import getCSVArrayFromFile from './commons/getCSVArrayFromFile';
 
 const Menu = ({ dispatch }) => {
+  const [ file, setFile ] = useState(null);
+
+  const importCSV = async () => {
+    const data = await getCSVArrayFromFile({ file });
+    const i=0;
+    // for (let i = 0; i < data.length; i++) {
+      console.log(data[i])
+      try {
+        await savePosition({ data: Immutable.fromJS(data[i]) });
+      } catch(e) {
+        console.error(e)
+      }
+    // }
+  };
+
   return (
     <div className='menu-container'>
       <button className='menu-item menu-item__addPosition' onClick={() => openModal({ dispatch })}>
         Add a Position
       </button>
+      <form className="menu-item">
+        <label htmlFor="upload">Import CSV</label>
+        <input
+          type="file"
+          id='upload'
+          accept=".csv"
+          onChange={(e) => {setFile(e.target.files[0]) }}
+          required
+        />
+        <button disabled={!file} onClick={importCSV}>Import</button>
+      </form>
     </div>
   )
 };
