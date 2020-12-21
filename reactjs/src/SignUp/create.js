5f8a3307e82a696e27ef8798743b3add0c0f535b
Yi Wen <hayafirst@protonmail.com>
2020-12-20 16:31:33 -0800

    Sgnup also creates a key

--- reactjs/src/SignUp/create.js
+++ reactjs/src/SignUp/create.js
@@ -1,9 +1,14 @@
 import auth from '../server/auth';
+import setEncryptionKey from '../encryption/setEncryptionKey';
+import history from '../commons/history';
 
 const create = ({ data, dispatch }) => {
   const email = data.get('email');
   const password = data.get('password');
-  auth().createUserWithEmailAndPassword(email, password);
+  auth().createUserWithEmailAndPassword(email, password).then(user => {
+    setEncryptionKey({ dispatch, password });
+    history.push('/');
+  });
 }
 
 export default create;
