2bdb195f109ae7b3060850ac81424a64963dc7a7
Yi Wen <hayafirst@protonmail.com>
2020-12-23 08:48:11 -0800

    Bug fix
    - Add a hover state to tables trs
    - Fix sorting bugs

--- src/position/sortPositions.js
+++ src/position/sortPositions.js
@@ -7,15 +7,18 @@ const func =({ positions, sortConditions }) => {
     const v1 = p1.get(field);
     const v2 = p2.get(field);
     let ascResult;
-    if (isNaN(Number(v1)) || Number(v1) !== 0) {
+    if (!isNaN(v1) && Number(v1) !== 0) {
       ascResult = Number(v1) - Number(v2);
     } else {
       ascResult = String(v1).localeCompare(String(v2));
+      if(v1 === '-SQ210115P215' || v2 === '-SQ210115P215') {
+        console.log(v1, v2, ascResult)
+      }
     }
     if (directionAsc || ascResult === 0) {
       return ascResult;
     }
-    return ascResult > 1 ? -1 : 1;
+    return ascResult > 0 ? -1 : 1;
   });
 }
 
