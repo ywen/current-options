d75e9dea980ef3e3e6f94c062599f85d3f9cebb5
Yi Wen <hayafirst@protonmail.com>
2021-01-12 08:19:00 -0800

    Remove Immutable in favor of immer

--- src/commons/composePositions.js
+++ src/commons/composePositions.js
@@ -1,14 +1,9 @@
-import Immutable from 'immutable';
-
 import getDecryptedData from '../encryption/getDecryptedData';
 import fillInferredValues from '../position/fillInferredValues';
 
 const composePositions = (state, action) => {
   const decrypted = getDecryptedData({ data: action.data });
-  const filled = decrypted.map(position => {
-    return fillInferredValues({ original: Immutable.fromJS(position) });
-  });
-  return Immutable.fromJS(filled);
+  return decrypted.map(position => fillInferredValues({ original: position }));
 };
 
 export default composePositions;
