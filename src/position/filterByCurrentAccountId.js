54ddc66b023f806815f33bd889024cefd4369d0b
Yi Wen <hayafirst@protonmail.com>
2021-01-12 23:53:26 -0800

    Fix list

--- src/position/filterByCurrentAccountId.js
+++ src/position/filterByCurrentAccountId.js
@@ -2,7 +2,9 @@ import { createSelector } from 'reselect';
 const filter = createSelector(
   state => state.positions,
   state => state.currentAccountId,
-  (positions, currentAccountId) => positions.filter(p => p.accountId === currentAccountId)
+  (positions, currentAccountId) => {
+    return positions.filter(p => p.accountId === currentAccountId)
+  }
 );
 
 export default filter;
