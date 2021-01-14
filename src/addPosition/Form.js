47cb800a5214c7497b38331f0f163bb8ea7b6e71
Yi Wen <hayafirst@protonmail.com>
2021-01-13 17:32:41 -0800

    Add formatDate

--- src/addPosition/Form.js
+++ src/addPosition/Form.js
@@ -3,13 +3,7 @@ import { connect } from 'react-redux';
 
 import PositionForm from '../position/Edit/Form';
 
-const Form = ({ dispatch, data }) => {
-  return (
-    <PositionForm
-      data={data}
-    />
-  )
-};
+const Form = ({ data }) => <PositionForm data={data} />
 
 export default connect(state => ({
   data: state.addPositionFormData,
