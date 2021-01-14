d1fe93e024554ead0a26b825918eb7559b894df4
Yi Wen <hayafirst@protonmail.com>
2021-01-13 22:37:47 -0800

    Be able to update

--- src/position/save.js
+++ src/position/save.js
@@ -5,8 +5,9 @@ import store from 'server/store';
 import field from './model';
 
 const savePosition = ({ data }) => {
+  console.log(data)
   const result = getEncryptedObjectFromMap({ data, fields: field.metaFields.concat('accountId') });
-  result.id = uuidv4();
+  result.id = data.id ? data.id : uuidv4();
   store.saveOpenPosition({ position: result });
 };
 
