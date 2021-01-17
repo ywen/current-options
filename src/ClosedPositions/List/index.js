3f9335df6f2c950d0ac54c5e8e6b34980956e856
Yi Wen <hayafirst@protonmail.com>
2021-01-16 22:22:00 -0800

    Remove unused files

--- src/ClosedPositions/List/index.js
+++ src/ClosedPositions/List/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import makeSortedPositionSelector from 'position/makeSortedPositionSelector';
 import PositionList from 'position/List';
 
-const List = ({}) => {
+const List = () => {
   const getSorted = makeSortedPositionSelector({
     sortConditionsKind: 'sortConditions',
     positionKind: 'closedPositions',
